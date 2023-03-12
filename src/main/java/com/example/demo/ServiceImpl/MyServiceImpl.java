f0cfd97ef97a318d0ae8b4af9aafdc011c9417af
kndk4u <kndk4u143@gmail.com>
2023-03-12 23:10:03 +0530

    added code for BAU2

--- src/main/java/com/example/demo/ServiceImpl/MyServiceImpl.java
+++ src/main/java/com/example/demo/ServiceImpl/MyServiceImpl.java
@@ -1,5 +1,14 @@
 package com.example.demo.ServiceImpl;
 
+import com.example.demo.daoImpl.SampleDaoImpl;
+
 public class MyServiceImpl {
+	
+	private SampleDaoImpl bauRepo;
+	
+	public void viewService(String msg) {
+		bauRepo.view(msg);
+	}
+	
 
 }
--- src/main/java/com/example/demo/daoImpl/SampleDaoImpl.java
+++ src/main/java/com/example/demo/daoImpl/SampleDaoImpl.java
@@ -2,4 +2,7 @@ package com.example.demo.daoImpl;
 
 public class SampleDaoImpl {
 
+	public void view(String msg) {
+		System.out.println(msg);
+	}
 }
